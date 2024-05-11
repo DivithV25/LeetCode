8b7afd35b1fa38a466115049d7f6015aa0732fd7
DivithV25 <divithv25@gmail.com>
2024-05-11 19:22:46 +0530

    Returns the number of students who are not able to eat the sandwich

--- 1700_StudentSandwich.cpp
+++ 1700_StudentSandwich.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+    int countStudents(vector<int> &students, vector<int> &sandwiches)
+    {
         int circlesandwich = 0;
         int squaresandwich = 0;
-        for (int i : students) {
+        for (int i : students)
+        {
             if (i)
                 squaresandwich++;
             else
                 circlesandwich++;
         }
-        for (int j : sandwiches) {
+        for (int j : sandwiches)
+        {
             if (j == 0 && circlesandwich == 0)
                 return squaresandwich;
             if (j == 1 && squaresandwich == 0)
