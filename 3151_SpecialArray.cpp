e5560998015f1e5573a479a3cdc8b111b6da38d8
DivithV25 <divithv25@gmail.com>
2024-06-08 21:24:06 +0530

    Finding wether the array is special or not

---
+++ 3151_SpecialArray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int parity(int x) {
+        if (x % 2 == 0)
+            return 1;
+        return 0;
+    }
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 1; i < n; i++) {
+
+            if ((parity(nums[i])) == (parity(nums[i - 1]))) {
+
+                return false;
+            }
+        }
+        return true;
+    }
+};
